bb2dd1158b9c26df20c3bcec5d398e68bac67953
Alex <pich.alex01@gmail.com>
2020-06-11 07:26:52 -0700

    Styled components, nprogress, edit some nav items

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,68 @@
 import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
+
+const theme = {
+  blue: "#63B3ED",
+  lightBlue: "#90CDF4",
+  grey: "#CBD5E0",
+  lightGrey: "#E2E8F0",
+  darkGrey: "#1A202C",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  boxShadow: "0 12px 24px 0 rgba(0,0,0,0.07)",
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${(props) => props.theme.darkGrey};
+`;
+
+const Inner = styled.div`
+  max-width: ${(props) => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+@font-face {
+  font-family: 'Montserrat', sans-serif;
+  src: url('/static/Montserrat-Regular.ttf')
+  format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+  html {
+    box-sizing:border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'Montserrat', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.darkgrey};
+  }
+`;
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
